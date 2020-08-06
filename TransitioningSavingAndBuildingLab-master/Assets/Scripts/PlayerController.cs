64e87a16e34b7fc8315a05a6b85dc125c15dfc88
Ritchy Lam <berkeley.gamedev@gmail.com>
2020-08-06 13:52:07 -0700

    Updated to 2019.3.14f

---
+++ TransitioningSavingAndBuildingLab-master/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("How quickly the player can move around.")]
+    private int m_Speed;
+    #endregion
+
+    #region Private Variables
+    // The amount of candy that the player currently has
+    private int p_NumCandies;
+    #endregion
+
+    #region Cached Components
+    private Rigidbody2D cc_Rb;
+    #endregion
+
+    #region Initialization Methods
+    private void Start()
+    {
+        cc_Rb = GetComponent<Rigidbody2D>();
+    }
+    #endregion
+
+    #region Main Updates
+    void Update () {
+        float vel = Input.GetAxis("Horizontal");
+        cc_Rb.velocity = new Vector2(vel * m_Speed, 0);
+	}
+    #endregion
+
+    #region Collision Methods
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //increase candy count and destroy the candy when the player collides with a candy
+        if (collision.gameObject.tag.Equals("candy"))
+        {
+            p_NumCandies += 1;
+            Destroy(collision.gameObject);
+        }
+    }
+    #endregion
+
+    #region Accessors and Mutators
+    public int NumCandies
+    {
+        get { return p_NumCandies; }
+        set { p_NumCandies = value; }
+    }
+    #endregion
+}
---
+++ TransitioningSavingAndBuildingLab-master/Assets/Scripts/candy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class Candy : MonoBehaviour
+{
+    #region Editor Variables
+    [SerializeField]
+    [Tooltip("The UI element that displays the number of candies.")]
+    private Text m_CandyText;
+    #endregion
+
+    #region Cached References
+    GameObject cr_Player;
+    PlayerController cr_Controller;
+    #endregion
+
+    #region Initialization Methods
+    private void Start ()
+    {
+        cr_Player = GameObject.Find("Player");
+        cr_Controller = cr_Player.GetComponent<PlayerController>();
+	}
+    #endregion
+
+    #region Main Updates
+    private void Update ()
+    {
+        //Continually updates the UI text for the number of candies collected
+        m_CandyText.text = "Candies: " + cr_Controller.NumCandies.ToString();
+	}
+    #endregion
+}
